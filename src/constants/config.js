36492285d69aa92b85938a792d7adcceab4f28a5
Roman Popovych <popovychjob@gmail.com>
2025-05-24 19:05:14 +0300

    update mine server url

--- src/constants/config.js
+++ src/constants/config.js
@@ -1,3 +1,2 @@
 export const API_BASE_URL = "https://api.forze.space";
-// export const MINECRAFT_URL = "mine.forze.space";
-export const MINECRAFT_URL = "xxxx.xxxx.xxxx";
+export const MINECRAFT_URL = "mine.forze.space";
